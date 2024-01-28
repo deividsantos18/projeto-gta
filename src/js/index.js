332d0302c9af446e2c2f919c6a9c3ce82b193fa3
Deivid Soares dos Santos <deividsantosccc@gmail.com>
2024-01-27 23:29:48 -0300

    Atualização

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataforma = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+    elementoPlataforma.classList.toggle("ativo")
+});
